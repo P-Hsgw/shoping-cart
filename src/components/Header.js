37e8b4e60baad6b46069394c783bd08930a7e583
P-Hsgw <contact@p-hsgw.com>
2021-05-28 11:48:49 +0900

    Add main page and update header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 function Header () {
   return(
-    <div className="justify-end items-center h-10 flex bg-prussian-blue ">
+    <div className="justify-end items-center h-12 flex bg-prussian-blue absolute w-screen ">
+      <p className="text-cultured mr-auto m-5 text-4xl">
+        STORE <span className="text-gamboge">DEMO</span>
+      </p>
       <ul>
-        <li className="inline-block m-5 text-cultured">
+        <li className="inline-block m-5 text-cultured text-2xl">
           Main
         </li>
-        <li className="inline-block m-5 text-cultured">
+        <li className="inline-block m-5 text-cultured text-2xl">
           Store
         </li>
-        <li className="inline-block m-5 text-cultured">
-          Cart
+        <li className="inline-block m-5 text-gamboge text-2xl ">
+          CART
         </li>
       </ul>
     </div>
---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+function Main () {
+  return(
+    <div className="bg-cultured h-screen flex justify-center items-center flex-col">
+      <h1 className="text-6xl text-rich-black">
+        THE ONLY STORE YOU NEED
+      </h1>
+      <h1 className="text-4xl mt-5 text-claret">
+        WE SELL STUFF
+      </h1>
+    </div>
+  )
+}
+
+export default Main
