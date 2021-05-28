f187b1345b2c42774bb339ee98c008bd78182143
P-Hsgw <contact@p-hsgw.com>
2021-05-28 10:12:33 +0900

    Add custom brand colors to Tailwind

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,14 @@
 function Header () {
   return(
-    <div className="justify-end items-center h-10 flex bg-yellow-600 ">
+    <div className="justify-end items-center h-10 flex bg-prussian-blue ">
       <ul>
-        <li className="inline-block m-5">
+        <li className="inline-block m-5 text-cultured">
           Main
         </li>
-        <li className="inline-block m-5">
+        <li className="inline-block m-5 text-cultured">
           Store
         </li>
-        <li className="inline-block m-5">
+        <li className="inline-block m-5 text-cultured">
           Cart
         </li>
       </ul>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
-  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "prussian-blue": "#002642",
+        "claret": "#840032",
+        "gamboge": "#E59500",
+        "cultured": "#F7F3F3",
+        "rich-black": "#02040F"
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
