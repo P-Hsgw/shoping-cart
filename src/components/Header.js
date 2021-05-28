9cebe1d1f14ce862be1539986c8aefa825f83e97
P-Hsgw <contact@p-hsgw.com>
2021-05-28 09:47:00 +0900

    Prepare project structure

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+function Header () {
+  return(
+    <div className="justify-end items-center h-10 flex bg-yellow-600 ">
+      <ul>
+        <li className="inline-block m-5">
+          Main
+        </li>
+        <li className="inline-block m-5">
+          Store
+        </li>
+        <li className="inline-block m-5">
+          Cart
+        </li>
+      </ul>
+    </div>
+  )
+}
+
+export default Header
